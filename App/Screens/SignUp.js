9483e8b85dcc223e0161dd99508127f7f64a3480
Apurba Sarkar <sarkar.apurba@hotmail.com>
2024-04-14 23:30:05 +0530

    custom form hook added

--- App/Screens/SignUp.js
+++ App/Screens/SignUp.js
@@ -8,160 +8,106 @@ import {
 import React, { useState } from "react";
 import createUser from "../Services/auth";
 import { useNavigation } from "@react-navigation/native";
-import { useController, useForm } from "react-hook-form";
+import { Controller, useController, useForm } from "react-hook-form";
 import { TextInput, TextInputMask, Button } from "react-native-paper";
 import theme from "../theme";
 
+// activeUnderlineColor={theme.colors.primary}
+// activeOutlineColor={theme.colors.primary}
+// textColor={theme.colors.primary}
+// label={field.name}
+// backgroundColor="#fff"
+
+const inputStyles = {
+  activeUnderlineColor: theme.colors.primary,
+  activeOutlineColor: theme.colors.primary,
+  textColor: theme.colors.primary,
+  backgroundColor: "#fff",
+};
+
 const SignUp = () => {
-  // const { control, handleSubmit } = useForm();
-  // const { field } = useController({ control, defaultValue: "" });
-
-  const navigation = useNavigation();
-  const [isCreating, setIsCreating] = useState(false);
-
-  const handleSignup = async () => {
-    // console.log("Username:", username);
-    setIsCreating(true);
-    await createUser(email, password);
-    setIsCreating(false);
-    console.log("Email:", email);
-    console.log("Password:", password);
+  const { control, handleSubmit, reset } = useForm();
+  const initialData = {
+    fullname: "",
+    birthyear: 0,
+    age: 2024 - this.birthyear,
+    bloodgroup: "",
+    mobile: 0,
+    email: "",
+    password: "",
   };
+
+  const handleSignup = async () => {};
 
   const onSubmit = (data) => {
+    // console.log(cusData);
     console.log(data);
+    // alert(JSON.stringify(data))
+    reset(initialData);
+    // e.target.reset();
   };
+
   return (
     <View style={styles.signup}>
       <Text style={styles.title}>Signup</Text>
-
-      {/* <TextInput
-        style={styles.input}
-        // value={username}
-        value={field.value}
-        onChangeText={field.onChange}
-        textColor={theme.colors.primary}
-        inputMode="text"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Full Name"
-        backgroundColor="#fff"
-        id="fullName"
+      <Controller
         control={control}
-
-        //  selectionColor={theme.colors.primary}
-      /> */}
-      {/* <TextInput
+        name="fullname"
         style={styles.input}
-        // value={username}
-        // onChangeText={(text) => setUsername(text)}
-        textColor={theme.colors.primary}
-        inputMode="numeric"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Birth Year"
-        backgroundColor="#fff"
-        keyboardType="number-pad"
-        id="birthYear"
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="full name"
+            value={value}
+            onChangeText={(value) => onChange(value)}
+            {...inputStyles}
+          />
+        )}
+      />
+      <Controller
         control={control}
-        //  selectionColor={theme.colors.primary}
-      />
-      <TextInput
+        name="fullname"
         style={styles.input}
-        // value={username}
-        // onChangeText={(text) => setUsername(text)}
-        textColor={theme.colors.primary}
-        inputMode="numeric"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Age"
-        backgroundColor="#fff"
-        disabled
-        id="age"
-        // {...register("age")}
-
-        //  selectionColor={theme.colors.primary}
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="full name"
+            value={value}
+            onChangeText={(value) => onChange(value)}
+            {...inputStyles}
+          />
+        )}
       />
-      <TextInput
+      <Controller
+        control={control}
+        name="fullname"
         style={styles.input}
-        // value={username}
-        // onChangeText={(text) => setUsername(text)}
-        textColor={theme.colors.primary}
-        inputMode="text"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Blood Group"
-        backgroundColor="#fff"
-        id="bloodGroup"
-        {...register("bloodGroup")}
-        // disabled
-
-        //  selectionColor={theme.colors.primary}
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="full name"
+            value={value}
+            onChangeText={(value) => onChange(value)}
+            {...inputStyles}
+          />
+        )}
       />
-      <TextInput
+      <Controller
+        control={control}
+        name="fullname"
         style={styles.input}
-        // value={username}
-        // onChangeText={(text) => setUsername(text)}
-        textColor={theme.colors.primary}
-        inputMode="numeric"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Mobile"
-        backgroundColor="#fff"
-        id="mobile"
-        {...register("mobile")}
-
-        // disabled
-
-        //  selectionColor={theme.colors.primary}
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="full name"
+            value={value}
+            onChangeText={(value) => onChange(value)}
+            {...inputStyles}
+          />
+        )}
       />
-      <TextInput
-        style={styles.input}
-        // value={username}
-        // onChangeText={(text) => setUsername(text)}
-        textColor={theme.colors.primary}
-        inputMode="email"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Email"
-        backgroundColor="#fff"
-        id="email"
-        {...register("email")}
-
-        // disabled
-
-        //  selectionColor={theme.colors.primary}
-      />
-
-      <TextInput
-        style={styles.input}
-        // value={username}
-        // onChangeText={(text) => setUsername(text)}
-        textColor={theme.colors.primary}
-        inputMode="text"
-        activeUnderlineColor={theme.colors.primary}
-        activeOutlineColor={theme.colors.primary}
-        label="Password"
-        backgroundColor="#fff"
-        id="password"
-        {...register("password")}
-
-        // disabled
-
-        //  selectionColor={theme.colors.primary}
-      /> */}
-      {/* <TouchableOpacity style={styles.button} onPress={handleSignup}>
-        <Text style={styles.buttonText}>Signup</Text>
-      </TouchableOpacity> */}
-      {/* <Button title="login" onPress={()=>navigation.replace("Login")}/>
-      {isCreating && <ActivityIndicator size="large" color="#0000ff" />} */}
+    
       <Button
         mode="contained"
-        // buttonColor={theme.colors.primary}
         textColor="#fff"
         contentStyle={{ height: 50 }}
-        // onPress={() => navigation.replace("SignUp")}
-        // onPress={handleSubmit(onSubmit)}
+        onPress={handleSubmit(onSubmit)}
         style={styles.cusButton}
         buttonColor={theme.colors.primary}
       >
@@ -170,15 +116,12 @@ const SignUp = () => {
       <Text> Already Registered. </Text>
       <Button
         mode="contained"
-        // buttonColor={theme.colors.primary}
         textColor="#fff"
         contentStyle={{ height: 50 }}
-        // onPress={() => navigation.replace("SignUp")}
-        // onPress={handleSubmit(onSubmit)}
         style={styles.cusButton}
         buttonColor={theme.colors.primary}
       >
-        SignUp
+        Log in
       </Button>
     </View>
   );
